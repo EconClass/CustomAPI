b0b70cf8102d1751599026a32797618ff0fe2885
EconClass <bizmail327@gmail.com>
2018-12-12 00:03:11 -0800

    added docsify

---
+++ server.js
@@ -0,0 +1,40 @@
+//=================================INITIAL=================================\\
+require('dotenv').config();
+const cookieParser = require('cookie-parser'),
+    jwt = require('jsonwebtoken'),
+    express = require('express'),
+    app = express(),
+    path = require('path'),
+    exphbs = require('express-handlebars').create({
+    layoutsDir: path.join(__dirname, "views/layouts"),
+    partialsDir: path.join(__dirname, "views/partials"),
+    defaultLayout: 'main',
+    extname: 'hbs'
+    }),
+    methodOverride = require('method-override'),
+    bodyParser = require('body-parser'),
+    port = process.env.PORT || 3000,
+    expressValidator = require('express-validator');
+
+
+//=================================MIDDLEWARE=================================\\
+
+// Cookie Parser
+app.use(cookieParser());
+
+// Handlebars
+app.engine('hbs', exphbs.engine)
+app.set('view engine', 'hbs');
+
+// Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator()); // Add after body parser initialization!
+
+// Method Override
+app.use(methodOverride('_method'));
+
+// Set db
+require('./data/reddit-db.js');
+
+app.listen(port);
