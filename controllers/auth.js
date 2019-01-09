090f2ffc0b7ef60e640b32ea3b6d84d9aa25928b
EconClass <bizmail327@gmail.com>
2019-01-09 14:27:41 -0800

    user routes started '

---
+++ controllers/auth.js
@@ -0,0 +1,12 @@
+module.exports = (app) => {
+    //=============LOG-IN USER=============\\
+    app.post()
+    //=============VIEW USER=============\\
+    app.get()
+    //=============CREATE USER=============\\
+    app.post()
+    //=============UPDATE USER=============\\
+    app.put()
+    //=============DELETE USER=============\\
+    app.delete()
+}
