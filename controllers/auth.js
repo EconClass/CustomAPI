08a990a98924ec561aa113a51ece27a1cee9fab6
EconClass <bizmail327@gmail.com>
2019-01-09 15:29:31 -0800

    added login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,7 +30,35 @@ module.exports = (app) => {
             });
     });
     //=============LOG-IN USER=============\\
-    app.post();
+    app.post('/login', (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+        // Find this user name
+        User.findOne({ username }, "username password")
+            .then(user => {
+                if (!user) {
+                    // User not found
+                    return res.status(401).send({ message: "Wrong Username or Password" });
+                };
+                // Check the password
+                user.comparePassword(password, (err, isMatch) => {
+                    if (!isMatch) {
+                        // Password does not match
+                        return res.status(401).send({ message: "Wrong Username or Password" });
+                    };
+                    // console.log(`user is ${user}`);
+                    // Create a token
+                    const token = jwt.sign({ _id: user._id, username: user.username }, secret, { expiresIn: "60 days" });
+
+                    // Set a cookie and redirect to root
+                    res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
+                    res.redirect("/");
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    });
     //=============UPDATE USER=============\\
     app.put();
     //=============DELETE USER=============\\
