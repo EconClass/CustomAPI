7573f2648c110ca33c249790b28d12e3dd9c22ec
EconClass <bizmail327@gmail.com>
2019-01-09 14:36:13 -0800

    login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,20 @@
+const User = require('../models/user.js');
+
 module.exports = (app) => {
     //=============LOG-IN USER=============\\
-    app.post()
+    app.post('/login', (req, res) => {
+        const user = new User(req.body);
+        user.save()
+        .then( user => {
+            let token = jwt.sign({ _id: user._id, username: user.username }, secret, { expiresIn: "60 days" });
+            res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+            res.redirect("/");
+        })
+        .catch((err)=>{
+            console.log(err.message);
+            return res.status(400).send({ err: err });
+        });
+    });
     //=============VIEW USER=============\\
     app.get()
     //=============CREATE USER=============\\
