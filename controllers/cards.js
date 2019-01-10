48fe3fe1315136be6dd6a70e4b43b045f16504f1
EconClass <bizmail327@gmail.com>
2019-01-10 15:14:38 -0800

    added add card

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,10 +1,33 @@
 const unirest = require('unirest'),
     Deck = require('../models/deck.js'),
-    User = require('../models/user.js');
+    Card = require('../models/card.js'),
+    KEY = process.env.APIKEY;
 
 module.exports = (app) => {
     //=============ADD CARD=============\\
-    app.post();
+    app.post('/user/decks/:id/card/:cardName', (req, res) => {
+        let dId = req.params.id;
+        Deck.findOne({_id: dId})
+        .exec(function (err, deck) {
+            unirest.get(`https://omgvamp-hearthstone-v1.p.mashape.com/cards/${req.params.cardName}`)
+            .header("X-Mashape-Key", KEY)
+            .header("Accept", "application/json")
+            .end(function (result) {
+                let info = result.body
+                const card = new Card({
+                    name: info.name,
+                    attack: info.attack,
+                    health: info.health,
+                    cost: info.cost,
+                    description: info.text,
+                    imgurl: info.img,
+                });
+                deck.cards.unshift(card);
+                deck.save();
+                return res.redirect(`/user/decks/${dId}`)
+            });
+        })
+    });
     //=============SEE CARD=============\\
     app.get();
     //=============REMOVE CARD=============\\
